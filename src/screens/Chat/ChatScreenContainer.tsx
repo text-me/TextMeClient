f65148605dd92a1cb8f992168dbd9c80f36151bc
Egor Voit <spamanet66@gmail.com>
2022-07-14 17:54:52 +0400

    feat: fetch old messages from server

--- src/screens/Chat/ChatScreenContainer.tsx
+++ src/screens/Chat/ChatScreenContainer.tsx
@@ -1,11 +1,22 @@
-import {MouseEvent, useState} from "react";
+import {useEffect, useState} from "react";
 import {ChatMessage} from "./types";
 import * as Styled from './style'
 import { MessageList } from "./MessageList";
 import { NewMessageInput } from "./NewMessageInput";
+import {fetch} from "@tauri-apps/api/http";
+import config from "../../config.json";
 
 export const ChatScreenContainer = () => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
+  
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const response = await fetch(`${config.serverUrl}/getMessages`);
+      setMessages(response.data as ChatMessage[]);
+    }
+    
+    fetchMessages();
+  }, []);
   
   const handleMessageSendClick = (newMessage: ChatMessage) => {
     setMessages([...messages, newMessage])
