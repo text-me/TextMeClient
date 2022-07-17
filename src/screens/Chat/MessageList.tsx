8d11fc146918fddd726e8d7951276852d8e82a9d
Egor Voit <spamanet66@gmail.com>
2022-07-17 17:20:11 +0400

    feat: auto scroll chat messages list to bottom

--- src/screens/Chat/MessageList.tsx
+++ src/screens/Chat/MessageList.tsx
@@ -1,14 +1,24 @@
 import {ScrollArea} from "@mantine/core";
 import { MessageItem } from "./MessageItem";
 import {ChatMessage} from "../../state/store";
+import {useEffect, useRef} from "react";
 
 type Props = {
   messages: ChatMessage[]
 }
 
 export const MessageList = ({messages}: Props) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  
+  // Always scroll to last message if messages have changed
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({top: Number.MAX_VALUE});
+    }
+  }, [messages])
+  
   return (
-    <ScrollArea>
+    <ScrollArea viewportRef={scrollRef}>
       {messages.map(({Text, ID}) => {
         return <MessageItem key={ID} text={Text} />
       })}
